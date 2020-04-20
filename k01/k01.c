84793f3e0723e7f95c5e4a48e08220e070310cdd
Yuta Hashiguchi <bb35319031@ms.nagasaki-u.ac.jp>
2020-04-20 16:17:33 +0900

    tst

--- k01/k01.c
+++ k01/k01.c
@@ -3,21 +3,13 @@
 #include <string.h>
 #include <math.h>
 
-int n;
-extern double ave_online(double val,double ave)
-{   ave=((n-1)*ave)/n + val /n ;
-       
-    return ave;
-}
-extern double var_online(double val,double ave,double _2ave)
-{   double var;
-    var=((n-1)*_2ave)/n + pow(val,2) - pow(((n-1)*ave)/n + val/n,2);
-    return var;
-}
+int n=1;
+extern double ave_online(double val,double ave);
+extern double var_online(double val,double ave,double _2ave);
 
 int main(void)
 {
-    double val,ave;
+    double val,ave=0,var=0,_2ave=0,s_var;
     char fname[FILENAME_MAX];
     char buf[300];
     FILE* fp;
@@ -35,15 +27,15 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
+         
          ave=ave_online(val,ave);
-         n=n+1;
+         _2ave=ave_online(pow(val,2),_2ave);
+         var=var_online(val,ave,_2ave);
 
-
-    
-
-
-
-    }
+         s_var=(n/(n-1))*var;
+         n++;
+        
+        }
 
     if(fclose(fp) == EOF){
         fputs("file close error\n",stderr);
@@ -51,10 +43,24 @@ int main(void)
     }
 
     printf("Average: %f\n",ave);
+    printf("Varience: %f\n",var);
+
+    printf("suitei_Average: %f\n",ave);
+    printf("suitei_Varience: %f\n",s_var);
+    
 
     return 0;
 
 
 }
 
-   
+extern double ave_online(double val,double ave)
+{   ave=((n-1)*ave)/n + val /n ;
+       
+    return ave;
+}
+extern double var_online(double val,double ave,double _2ave)
+{   double var;
+    var=((n-1)*_2ave)/n + pow(val,2) - pow(((n-1)*ave)/n + val/n,2);
+    return var;
+}
