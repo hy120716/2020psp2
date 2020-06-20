1629f9bff5b50d200afcedbed087fd31e64e250a
Yuta Hashiguchi <bb35319031@ms.nagasaki-u.ac.jp>
2020-06-20 16:34:56 +0900

    ここまで

--- k04/k04.c
+++ k04/k04.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+struct data
+{   int ID;
+    int gender;
+    double heights;
+};
+
+int main(void)
+{
+    char fname[FILENAME_MAX];
+    char buf[256];
+    FILE* fp;
+
+    printf("input the filename of sample heights ?:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample heights: %s\n",fname);
+
+    fp = fopen(fname,"r");
+    if(fp==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+   printf("input the filename of sample ID ?:");
+    fgets(fname,sizeof(fname),stdin);
+    fname[strlen(fname)-1] = '\0';
+    printf("the filename of sample ID: %s\n",fname);
+
+    fp = fopen(fname,"r");
+    if(fp==NULL){
+        fputs("File open error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
 
