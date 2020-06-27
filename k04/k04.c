ef26fda8fa09117d15855279bd34ac0886601b1a
Yuta Hashiguchi <bb35319031@ms.nagasaki-u.ac.jp>
2020-06-27 19:38:51 +0900

    ここまで

--- k04/k04.c
+++ k04/k04.c
@@ -14,6 +14,11 @@ int main(void)
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
+    
+    int i=0;
+    struct data sample[15];
+    int g,id;
+    double h;
 
     printf("input the filename of sample heights ?:");
     fgets(fname,sizeof(fname),stdin);
@@ -25,6 +30,22 @@ int main(void)
         fputs("File open error\n",stderr);
         exit(EXIT_FAILURE);
     }
+    
+    while(fgets(buf,sizeof(buf),fp) != NULL){
+        sscanf(buf,"%d , %lf",&g,&h);
+        
+        sample[i-1].gender = g;
+        sample[i-1].heights = h;
+        i=i+1;
+
+    }
+
+
+    if(fclose(fp) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
 
    printf("input the filename of sample ID ?:");
     fgets(fname,sizeof(fname),stdin);
@@ -37,4 +58,26 @@ int main(void)
         exit(EXIT_FAILURE);
     }
 
+    i=0;
+
+    while(fgets(buf,sizeof(buf),fp) != NULL){
+        sscanf(buf,"%d",&id);
+
+        sample[i].ID = id;
+        i=i+1;
+
+    }
+
+
+    if(fclose(fp) == EOF){
+        fputs("file close error\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+
+printf("%d/", sample[0].ID);
+printf("%d/", sample[0].gender);
+printf("%lf\n", sample[0].heights);
+
+return 0;
+}
 
